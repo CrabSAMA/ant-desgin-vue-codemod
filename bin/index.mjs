933096cca3c4f3fae35746e5d2a8a2b85e89fa91
crab.huang <crab.huang@huolala.cn>
2024-10-31 18:03:58 +0800

    feat: 新增 style 样式导入检测

--- bin/index.mjs
+++ bin/index.mjs
@@ -5,16 +5,20 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 async function run() {
-  const config = path.resolve(__dirname, '../sgconfig.yml')
   const sg = path.resolve(__dirname, '../node_modules/.bin/ast-grep')
+  const getConfig = (type) => path.resolve(__dirname, `../${type}-sgconfig.yml`)
   
   const cwd = process.cwd()
   console.log(`当前执行目录：${cwd}\n`);
   
   try {
-    console.log('开始执行...');
-    await $`${sg} scan -c ${config} -U --tracing 'summary'`
-    console.log('执行完成！');
+    console.log('开始对 prop break change 进行代码重写...');
+    await $`${sg} scan -c ${getConfig('prop')} -U --tracing 'summary'`
+    console.log('prop break change 代码重写执行完成！\n');
+
+    console.log('ant-design-vue4 移除了 style 样式导入，开始检测...');
+    await $`${sg} scan -c ${getConfig('import-style')}`
+    console.log('style 样式导入代码检测完成！');
   } catch (error) {
     
   }
---
+++ test/import-style.ts
@@ -0,0 +1,12 @@
+import 'ant-design-vue/dist/antd.less';
+import "ant-design-vue/dist/antd.less";
+import 'ant-design-vue/dist/antd.css';
+import "ant-design-vue/dist/antd.css";
+
+// 按需引入
+import 'ant-design-vue/lib/button/style'
+import 'ant-design-vue/lib/button/style/index.js'
+import 'ant-design-vue/lib/button/style/css'
+import 'ant-design-vue/lib/button/style/css.js'
+import 'ant-design-vue/es/message/style/index.css'
+import 'ant-design-vue/es/message/style/index.less'
